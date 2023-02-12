91ec50a354f606a53b0b243651907a141be94f91
Devang Thesiya <devangthesiya16@gmail.com>
2023-02-12 18:08:05 +0530

    Add user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # get 'articles', to: 'pages#articles'
   resources :articles
-
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
   # resources provides all the REST-ful routes to rails resources
 end
